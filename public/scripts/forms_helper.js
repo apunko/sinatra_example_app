d8a20cf6ddda5edee4db3b00768327eddf0d125b
anton <ant.punko@gmail.com>
2017-07-26 01:37:13 +0300

    Added item s views and add action

--- public/scripts/forms_helper.js
+++ public/scripts/forms_helper.js
@@ -1,5 +1,6 @@
 var Forms = Forms || {};
 Forms.TaskList = Forms.TaskList || {};
+Forms.Items = Forms.Items || {};
 
 Forms.TaskList.CreateForm = (function() {
   var _form_selector; 
@@ -15,6 +16,35 @@ Forms.TaskList.CreateForm = (function() {
         e.preventDefault();
         $.post({
           url : "/create_task_list",
+          dataType: 'json',
+          data : $(_form_selector).serialize()
+        })
+        .done(function() {
+          alert( "success" );
+        })
+        .fail(function() {
+          alert( "error" );
+        });
+    }
+  }
+
+  return createForm;
+})();
+
+Forms.Items.CreateForm = (function() {
+  var _form_selector; 
+
+  var createForm = {
+    Initialize: function(form_selector) {
+      _form_selector = form_selector;
+      $(form_selector).submit(Forms.Items.CreateForm.OnCreate);
+    },
+
+    OnCreate: function (e) {
+        debugger;
+        e.preventDefault();
+        $.post({
+          url : "/add_item",
           dataType: 'json',
           data : $(_form_selector).serialize()
         })
